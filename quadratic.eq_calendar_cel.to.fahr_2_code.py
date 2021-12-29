ea40e518a1f45cc60171056e3b2d7f4c916b2a5f
Sourav Kumar Gupta <54281418+SouravKumarGupta@users.noreply.github.com>
2021-12-29 13:33:14 +0530

    Update and rename Assignment_2_code.py to quadratic.eq_calendar_cel.to.fahr_2_code.py

--- quadratic.eq_calendar_cel.to.fahr_2_code.py
+++ quadratic.eq_calendar_cel.to.fahr_2_code.py
@@ -6,22 +6,22 @@ Created on Sun Dec 26 20:55:41 2021
 """
 
 # # #1.	Write a Python program to convert kilometers to miles?
-# km=float(input("Enter value in kilometer : "))
-# # 1km=0.6213 miles
-# m=0.6213
+km=float(input("Enter value in kilometer : "))
+# 1km=0.6213 miles
+m=0.6213
 
-# print(round(km*m,2),"Miles")
+print(round(km*m,2),"Miles")
 
-# # #2.	Write a Python program to convert Celsius to Fahrenheit?
-# celsius=float(input("Enter the value in celsius : "))
-# # calculate fahrenheit
-# fahrenheit = (celsius * 1.8) + 32
-# print(fahrenheit,"fahrenheit")
+#2.	Write a Python program to convert Celsius to Fahrenheit?
+celsius=float(input("Enter the value in celsius : "))
+calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print(fahrenheit,"fahrenheit")
 
-# #3.	Write a Python program to display calendar?
-# import calendar
-# yy=2021
-# print(calendar.calendar(yy))
+#3.	Write a Python program to display calendar?
+import calendar
+yy=2021
+print(calendar.calendar(yy))
 
 #4.	Write a Python program to solve quadratic equation?
 import cmath,math
@@ -42,9 +42,9 @@ else:
     print("Roots are real and distinct :",s1,s2)
 
 #5.	Write a Python program to swap two variables without temp variable?
-# x=input("Enter 1st value : ")
-# y=input("Enter 2nd value : ")
+x=input("Enter 1st value : ")
+y=input("Enter 2nd value : ")
 
-# x,y=y,x
+x,y=y,x
 
-# print("Reveresd values are : ",x,y)
+print("Reveresd values are : ",x,y)
